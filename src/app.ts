94bfac1ad573593594e6aee013bb386451dded71
Mamun-Ashraf <ashrafdkia@gmail.com>
2024-05-09 19:27:04 +0600

    setup cors, parser

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
-import express from "express";
-const app = express();
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const app: Application = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Working successfully!");
 });
 
 export default app;
